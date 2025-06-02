bba0186adefa0222d683aea3d362c3906a703c4b
jucheolkang <jucheol9877@naver.com>
2025-06-02 12:06:43 +0900

    회원가입 주소 변경

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById("loginBtn").addEventListener("click", () => {
-    location.href = "https://ehdjfkr03.github.io/userOTP/signIn/signIn.html"; 
+    location.href = "/userOTP/signIn/signIn.html"; 
     // 로그인 페이지로 이동
     // html(화면 코드)에 id로 loginBtn 가진 버튼을 클릭하면 동작
   });
   
   document.getElementById("signupBtn").addEventListener("click", () => {
-    location.href = "../signUp/signUp.html";
+    location.href = "/userOTP/signUp/signUp.html";
     // 회원가입 페이지로 이동
   });
   
